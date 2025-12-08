8f82434a5a47236589282c079b8f81be2b8bc3b2
min <kmins97@naver.com>
2025-12-08 22:50:22 +0900

    수정

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -18,16 +18,13 @@ export const authApi = {
   },
 
   login: async (email, password) => {
-    const requestBody = {
-      email: email,
-      password: password,
-    };
+    const requestBody = {email, password}
     const response = await api.post('/auth/login', requestBody);
     return response.data;
   },
 
   checkEmailDuplicate: async (email) => {
-    // const response = await api.get(`/auth/check-email?email=${email}`);
+    const response = await api.get(`/auth/check-email?email=${email}`);
     // return response.data; // (true면 중복, false면 사용가능)
 
     console.log(`[임시] 이메일 중복 확인 통과: ${email}`);
