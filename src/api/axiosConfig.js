04b2812ad29e6497456a0b5df86e83a5be636bbc
min <kmins97@naver.com>
2025-12-08 23:25:16 +0900

    vercel 용

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 console.log("-------------", process.env.REACT_APP_API_BASE_URL)
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api',
+  baseURL: '/api',
   
   timeout: 10000,
   headers: {
