41a21c75d5555ce99e44748c3d9875443a76a3b4
min <kmins97@naver.com>
2025-12-08 23:44:41 +0900

    링크 숨기기

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log("-------------", process.env.REACT_APP_API_BASE_URL)
-
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api',
   // baseURL : '/api',
